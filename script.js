b8746445f30a4beb2e5b8297dd24e7cba46f526c
Rashad <akhirrashad@gmail.com>
2024-02-02 16:01:02 -0800

    Step-59

--- script.js
+++ script.js
@@ -37,11 +37,7 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function update(location){
-    
-}
-
-function goTown(){   
+function update(location) {
     button1.innerText = "Go to store";
     button2.innerText = "Go to cave";
     button3.innerText = "Fight dragon";
@@ -53,19 +49,14 @@ function goTown(){
     text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
 
-function goStore(){
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-
-    text.innerText = "You enter the store.";
+function goTown() {   
 }
 
-function goCave(){
+function goStore() {
+
+}
+
+function goCave() {
     console.log("Going to cave.");
 }
 
@@ -73,7 +64,9 @@ function fightDragon() {
     console.log("Fighting dragon.");
 }
 
-function buyHealth(){   
+function buyHealth() {
+
 }
-function buyWeapon(){   
+function buyWeapon() {   
+
 }
